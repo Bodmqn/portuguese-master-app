786af863daf5212e4cb48a156cb20ae4f8a0dec1
Bodmqn <bodmqn@users.noreply.github.com>
2026-09-19 21:17:32 +0100

    Bump service-worker cache to v2 so Ate/Até fix goes live (purge stale data.js)

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,12 @@
 self.addEventListener('install', e=>{
 e.waitUntil(
-caches.open('pt-lessons-v1').then(cache=>{
+caches.open('pt-lessons-v2').then(cache=>{
 return cache.addAll(['index.html','data.js','manifest.json']);
 })
 );
 });
 self.addEventListener('activate', e=>{
 e.waitUntil(
-caches.keys().then(keys=>Promise.all(keys.filter(k=>k!=='pt-lessons-v1').map(k=>caches.delete(k))))
+caches.keys().then(keys=>Promise.all(keys.filter(k=>k!=='pt-lessons-v2').map(k=>caches.delete(k))))
 );
 });
